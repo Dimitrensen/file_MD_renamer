3f16bb67a086cfe0c1a94d628639e027591aa3a3
Dimitris Alexiou <97767993+Dimitrensen@users.noreply.github.com>
2024-06-18 11:34:18 +0000

    script first part fixed

--- Script.sh
+++ Script.sh
@@ -3,4 +3,4 @@
 #make a script which copies all the files from Files to Backup directory and pastes them inside backup folder with their name convension changed
 
 echo test
-cp -R Files/* /Backup/
+cp -R Files/* Backup/
