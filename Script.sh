438210647c239c92c05175856ec3a74e898678aa
dimitrensen <mitsen@hotmail.gr>
2024-06-18 13:05:53 +0200

    Test

--- Script.sh
+++ Script.sh
@@ -2,4 +2,5 @@
 
 #make a script which copies all the files from Files to Backup directory and pastes them inside backup folder with their name convension changed
 
+echo test
 cp -R Files/* /Backup/
