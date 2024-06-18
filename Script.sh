489c9e49ed6e7c6d04b2474d2ba4706d16124fbb
dimitrensen <mitsen@hotmail.gr>
2024-06-18 16:49:00 +0200

    script last steps

--- Script.sh
+++ Script.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#make a script which copies all the files from Files to Backup directory and pastes them inside backup folder with their name convension c.hanged
-
 set -ex
 
 if [ ! -d "./Backup" ]; then
@@ -14,10 +12,3 @@ for file in $(ls ./Files); do
     md5=$(md5sum ./Files/$file | cut -d ' ' -f 1)
     mv ./Files/$file ./Files/$md5
 done
-
-# echo $md5
-# firstName=$(echo ${name} | cut -d ' ' -f 1)
-# cp -R Files/* Backup/
-
-#output somewhere temporarily the md5sum of every file of Files directory, take the output and create a file for each and every md5 that is output in the Backup folder
-##can be put in a function
